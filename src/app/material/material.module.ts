ab7a883eee2e55367f0218e3035ac406069fec27
rau <c.sa.r@hotmail.com>
2018-03-08 18:09:45 -0600

    se le agrego un componente lista y se acomodo para que lo use material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule 
   ],exports:[
-    MatButtonModule
+    MatButtonModule,
+    ListaComponent
   ],
-  declarations: []
+  declarations: [ListaComponent]
 })
 export class MaterialModule { }
