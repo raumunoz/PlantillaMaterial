2cfeb1a5132b79a25ad342d1e8d5eed0a71593f2
rau <c.sa.r@hotmail.com>
2018-04-16 15:17:56 -0500

    se realizaron cambios en el envoriment.prod.ts para entrar a fase de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBioQfjuIXtg6HFxtXEIlwGzQugsL3hMpA",
+    authDomain: "comentarios-24b58.firebaseapp.com",
+    databaseURL: "https://comentarios-24b58.firebaseio.com",
+    projectId: "comentarios-24b58",
+    storageBucket: "comentarios-24b58.appspot.com",
+    messagingSenderId: "628291055878"
+  }
 };
